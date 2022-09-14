27742abb1b1f36bb424ce69fac60b192d84a9f2d
Jateen Sharma <jateen.sharma@thoughtworks.com>
2022-09-14 14:41:01 +0530

    [Jatin] first Checkin

---
+++ src/test/java/com/jatin/RegisterUserTest.java
@@ -0,0 +1,14 @@
+package com.jatin;
+
+import org.testng.annotations.Test;
+
+
+public class RegisterUserTest {
+    
+    @Test
+    public void shouldBeAbleToRegister()
+    {
+        System.out.println("RegisterUserTest should be able to register");
+        System.out.println();
+    }
+}
